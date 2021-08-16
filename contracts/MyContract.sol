ed6844685171e871ad22a413a9ab13afe04aeb7b
Friskybunnies <friskybunnies@gmail.com>
2021-08-16 11:39:11 -0400

    Project Setup

---
+++ contracts/MyContract.sol
@@ -0,0 +1,5 @@
+pragma solidity ^0.5.0;
+
+contract MyContract {
+    uint public taskCount = 0;
+}
--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import React, { Component } from 'react'
-import Web3 from 'web3'
-import './App.css'
-import { TODO_LIST_ABI, TODO_LIST_ADDRESS } from './config'
-import TodoList from './TodoList'
+import React, { Component } from 'react';
+import Web3 from 'web3';
+import './App.css';
+import { TODO_LIST_ABI, TODO_LIST_ADDRESS } from './config';
+import TodoList from './TodoList';
 
 class App extends Component {
-  componentWillMount() {
-    this.loadBlockchainData()
+  componentDidMount() {
+    this.loadBlockchainData();
   }
 
   async loadBlockchainData() {
-    const web3 = new Web3(Web3.givenProvider || "http://localhost:8545")
-    const accounts = await web3.eth.getAccounts()
+    const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+    const req = await web3.eth.requestAccounts();
+    const network = await web3.eth.net.getNetworkType();
+    console.log("network", network);
+    const accounts = await web3.eth.getAccounts();
+    console.log("account", accounts[0]);
     this.setState({ account: accounts[0] })
     const todoList = new web3.eth.Contract(TODO_LIST_ABI, TODO_LIST_ADDRESS)
     this.setState({ todoList })
@@ -42,24 +46,24 @@ class App extends Component {
   createTask(content) {
     this.setState({ loading: true })
     this.state.todoList.methods.createTask(content).send({ from: this.state.account })
-    .once('receipt', (receipt) => {
-      this.setState({ loading: false })
-    })
+      .once('receipt', (receipt) => {
+        this.setState({ loading: false })
+      })
   }
 
   toggleCompleted(taskId) {
     this.setState({ loading: true })
     this.state.todoList.methods.toggleCompleted(taskId).send({ from: this.state.account })
-    .once('receipt', (receipt) => {
-      this.setState({ loading: false })
-    })
+      .once('receipt', (receipt) => {
+        this.setState({ loading: false })
+      })
   }
 
   render() {
     return (
       <div>
         <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="http://www.dappuniversity.com/free-download" target="_blank">Dapp University | Todo List</a>
+          <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="" target="_blank">Decent Labs Assignment | Ethereum Smart Contract App</a>
           <ul className="navbar-nav px-3">
             <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
               <small><a className="nav-link" href="#"><span id="account"></span></a></small>
@@ -69,7 +73,7 @@ class App extends Component {
         <div className="container-fluid">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex justify-content-center">
-              { this.state.loading
+              {this.state.loading
                 ? <div id="loader" className="text-center"><p className="text-center">Loading...</p></div>
                 : <TodoList
                   tasks={this.state.tasks}
